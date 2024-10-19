3a788e6f623dd468b9f293efe72abe0ab60fb533
shital-kendre <shital0895@gmail.com>
2024-10-20 01:07:30 +0530

    Added configuration file

---
+++ config.py
@@ -0,0 +1,7 @@
+FLASK_PORT_NUMBER = 5006
+
+CSV_FILE_NAME = r"data\Salary_Data.csv" 
+
+MODEL_FILE_NAME = r"artifacts\Salary_linear_reg_model.pkl"
+
+# COLUMN_DATA_JSON = r"artifacts\column_data.json"
