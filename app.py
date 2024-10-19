65ee539851c3a436c76e13fec3f4b56479bd835e
shital-kendre <shital0895@gmail.com>
2024-10-20 01:05:56 +0530

    Added app.py file

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template, jsonify
+from utils import SalaryPrediction, load_dataset
+import config
+from flask_cors import CORS
+
+df = load_dataset()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/prediction', methods = ['POST'])
+def prediction():
+    data = request.form
+    print(data)
+
+    YearsExperience = float(data['YearsExperience'])
+
+    sal_pred = SalaryPrediction()
+
+    predicted_salary = sal_pred.get_salary(YearsExperience)
+
+    print(f"Predicted salary : {predicted_salary}" ) #Debugging log
+    return jsonify({"predicted salary" : predicted_salary})
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = config.FLASK_PORT_NUMBER, debug=True)
